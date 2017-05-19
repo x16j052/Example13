e18cc92ad774044d093ea09918a964a2daaecc50
WIN_NT\x16j052 <x16j052@fjb-ac.jp>
2017-05-19 12:39:29 +0900

    no message

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Example13
 {
-    partial class Form1
+    partial class btnExec
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-22, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(513, 168);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ＢＵットン";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "btnExec";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WHILEループ";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace Example13
 {
-    public partial class Form1 : Form
+    public partial class btnExec : Form
     {
-        public Form1()
+        public btnExec()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = DialogResult.Yes;
+            while(result == DialogResult.Yes)
+            {
+                result = MessageBox.Show("無限にくりｋりり", "確認",MessageBoxButtons.YesNo);
+                
+                System.Threading.Thread.Sleep(1000);
+            }
+            MessageBox.Show("whileループを終了しました", "情報");
         }
     }
 }
